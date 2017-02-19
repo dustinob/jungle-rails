63a8b8353ee520a2f9591b382c115b87b45fb742
Dustin OBrien <dcobrien82@gmail.com>
2017-02-19 19:24:43 +0000

    authentication for admin dropdown complete, added to admin controller, added logged_in? handler, added logged_in? erb to nav_bar partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
+
+  protected
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['HTTP_AUTH_USERNAME'] && password == ENV['HTTP_AUTH_PASSWORD']
+      end
+    end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
